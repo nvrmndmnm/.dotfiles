604cafe331487dfef786b3db4d427c6583e76f7a
nvm <s.avkhadeev@gmail.com>
2024-09-21 14:42:55 +0500

    fix bg script

--- i3/.config/i3/scripts/set-bg.sh
+++ i3/.config/i3/scripts/set-bg.sh
@@ -3,7 +3,7 @@
 HOSTNAME=$(hostnamectl hostname)
 
 if [ "$HOSTNAME" == "aero" ]; then
-    feh --bg-center .dotfiles/i3/wp.jpg .dotfiles/i3/brs.jpg
+    feh --bg-center ~/.dotfiles/i3/wp.jpg ~/.dotfiles/i3/brs.jpg
 elif [ "$HOSTNAME" == "zero" ]; then
     xwinwrap -g 1920x1080+3840 -ov -ni -s -nf -- mpv --speed=0.5 --loop --wid=%WID special_shapes.mkv
 fi
