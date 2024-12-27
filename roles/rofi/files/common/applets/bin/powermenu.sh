9abe03b039927502f0d2e8e153a130ccfd11ebcc
nvm <s.avkhadeev@gmail.com>
2024-12-27 21:40:55 +0500

    remove confirmation from power applet

--- roles/rofi/files/common/applets/bin/powermenu.sh
+++ roles/rofi/files/common/applets/bin/powermenu.sh
@@ -45,48 +45,20 @@ run_rofi() {
 	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6" | rofi_cmd
 }
 
-# Confirmation CMD
-confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${theme}
-}
-
-# Ask for confirmation
-confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
-}
-
-# Confirm and execute
-confirm_run () {	
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-        ${1} && ${2} && ${3}
-    else
-        exit
-    fi	
-}
-
 # Execute Command
 run_cmd() {
 	if [[ "$1" == '--opt1' ]]; then
 		betterlockscreen -l
 	elif [[ "$1" == '--opt2' ]]; then
-		confirm_run 'kill -9 -1'
+		'kill -9 -1'
 	elif [[ "$1" == '--opt3' ]]; then
-		confirm_run 'mpc -q pause' 'amixer set Master mute' 'systemctl suspend'
+		'mpc -q pause' 'amixer set Master mute' 'systemctl suspend'
 	elif [[ "$1" == '--opt4' ]]; then
-		confirm_run 'systemctl hibernate'
+		'systemctl hibernate'
 	elif [[ "$1" == '--opt5' ]]; then
-		confirm_run 'systemctl reboot'
+		'systemctl reboot'
 	elif [[ "$1" == '--opt6' ]]; then
-		confirm_run 'systemctl poweroff'
+		'systemctl poweroff'
 	fi
 }
 
